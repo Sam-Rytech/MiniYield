380a1aa35465b8038225655bf567c020281f37c2
Omolayo Temitayo <temitayoomolayo@gmail.com>
2025-08-09 13:01:37 +0100

    Updated hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,8 @@
-/** @type import('hardhat/config').HardhatUserConfig */
-module.exports = {
-  solidity: "0.8.28",
+import "@nomicfoundation/hardhat-toolbox";
+
+export const solidity = "0.8.28";
+export const paths = {
+  sources: "../contracts/src",
+  tests: "../contracts/test",
+  scripts: "../contracts/scripts"
 };
